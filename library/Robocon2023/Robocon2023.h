52e12299a23353e47e4b0101b36545cbfe79585e
Northim0307 <121028514+Northim0307@users.noreply.github.com>
2023-05-12 16:56:52 +0800

    Add files via upload

--- library/Robocon2023/Robocon2023.h
+++ library/Robocon2023/Robocon2023.h
@@ -612,13 +612,23 @@ void ICM20948_GET_READING(double *yaw)
 	
 void Setpoint_Adjust_Yaw (double yaw, int motor_num, int motor_direction, int setpoint_m[4], const int speed_chg_per_yaw ) 
 {
+	/*use to limit the speed change due to yaw*/
+	if(yaw>20)
+	{
+		yaw=20;
+	}
+	else if(yaw<-20)
+	{
+		yaw=20;
+	}
+	
 		if(motor_direction == CLOCKWISE)
 		{
-			setpoint_m[motor_num] = setpoint_m[motor_num] + (yaw * speed_chg_per_yaw)/3;
+			setpoint_m[motor_num] = setpoint_m[motor_num] - (yaw * speed_chg_per_yaw)/3;
 		}
 		else if(motor_direction == COUNTER_CLOCKWISE)
 		{
-			setpoint_m[motor_num] = setpoint_m[motor_num] - (yaw * speed_chg_per_yaw)/3 ;
+			setpoint_m[motor_num] = setpoint_m[motor_num] + (yaw * speed_chg_per_yaw)/3 ;
 		}
 	return;
 }	  
@@ -1004,8 +1014,8 @@ if(*magnitude <30 )
 		ratio = *angle / 90;
 		setpoint_m[0] = optimum_speed_m[0]+12;
 		setpoint_m[1] = optimum_speed_m[1]+12;
-		setpoint_m[2] = optimum_speed_m[2]-15;
-		setpoint_m[3] = optimum_speed_m[3]-15;
+		setpoint_m[2] = optimum_speed_m[2]-12;
+		setpoint_m[3] = optimum_speed_m[3]-12;
 		
 	}
 	
@@ -1015,7 +1025,7 @@ if(*magnitude <30 )
 		ratio = *angle / 90;
 		setpoint_m[0] = optimum_speed_m[0]+12;
 		setpoint_m[1] = optimum_speed_m[1];
-		setpoint_m[2] = optimum_speed_m[2]-15;
+		setpoint_m[2] = optimum_speed_m[2]-12;
 		setpoint_m[3] = optimum_speed_m[3];
 	}
 	
@@ -1024,8 +1034,8 @@ if(*magnitude <30 )
 		// *robot_dir_now = FORWARD;
 		ratio = *angle / 90;
 		setpoint_m[0] = optimum_speed_m[0]+12;
-		setpoint_m[1] = optimum_speed_m[1]-15;
-		setpoint_m[2] = optimum_speed_m[2]-15;
+		setpoint_m[1] = optimum_speed_m[1]-12;
+		setpoint_m[2] = optimum_speed_m[2]-12;
 		setpoint_m[3] = optimum_speed_m[3]+12;
 	}
 	
@@ -1034,8 +1044,8 @@ if(*magnitude <30 )
 		// *robot_dir_now = FORWARD;
 		ratio = (*angle-90) / 90;
 		setpoint_m[0] = optimum_speed_m[0]+12;
-		setpoint_m[1] = optimum_speed_m[1]-15;
-		setpoint_m[2] = optimum_speed_m[2]-15;
+		setpoint_m[1] = optimum_speed_m[1]-12;
+		setpoint_m[2] = optimum_speed_m[2]-12;
 		setpoint_m[3] = optimum_speed_m[3]+12;
 	}
 	
@@ -1043,7 +1053,7 @@ if(*magnitude <30 )
 	{
 		// *robot_dir_now = UPWARD_LEFT;
 		setpoint_m[0] = optimum_speed_m[0];
-		setpoint_m[1] = optimum_speed_m[1]-15;
+		setpoint_m[1] = optimum_speed_m[1]-12;
 		setpoint_m[2] = optimum_speed_m[2];
 		setpoint_m[3] = optimum_speed_m[3]+12;
 	}
@@ -1052,8 +1062,8 @@ if(*magnitude <30 )
 	{
 		// *robot_dir_now = LEFTWARD;
 		ratio = (*angle-90) / 90;
-		setpoint_m[0] = optimum_speed_m[0]-15;
-		setpoint_m[1] = optimum_speed_m[1]-15;
+		setpoint_m[0] = optimum_speed_m[0]-12;
+		setpoint_m[1] = optimum_speed_m[1]-12;
 		setpoint_m[2] = optimum_speed_m[2]+12;
 		setpoint_m[3] = optimum_speed_m[3]+12;
 	}
@@ -1062,7 +1072,7 @@ if(*magnitude <30 )
 	{
 		// *robot_dir_now = LEFTWARD;
 		ratio = (*angle+180) / 90;
-		setpoint_m[0] = optimum_speed_m[0]-15;
+		setpoint_m[0] = optimum_speed_m[0]-12;
 		setpoint_m[1] = optimum_speed_m[1]-15;
 		setpoint_m[2] = optimum_speed_m[2]+12;
 		setpoint_m[3] = optimum_speed_m[3]+12;
@@ -1072,7 +1082,7 @@ if(*magnitude <30 )
 	{
 		// *robot_dir_now = DOWNWARD_LEFT;
 		ratio = (*angle+180) / 90;
-		setpoint_m[0] = optimum_speed_m[0]-15;
+		setpoint_m[0] = optimum_speed_m[0]-12;
 		setpoint_m[1] = optimum_speed_m[1];
 		setpoint_m[2] = optimum_speed_m[2]+12;
 		setpoint_m[3] = optimum_speed_m[3];
@@ -1082,20 +1092,20 @@ if(*magnitude <30 )
 	{
 		// *robot_dir_now = BACKWARD;
 		ratio = (*angle+180) / 90;
-		setpoint_m[0] = optimum_speed_m[0]-15;
+		setpoint_m[0] = optimum_speed_m[0]-12;
 		setpoint_m[1] = optimum_speed_m[1]+12;
 		setpoint_m[2] = optimum_speed_m[2]+12;
-		setpoint_m[3] = optimum_speed_m[3]-15;
+		setpoint_m[3] = optimum_speed_m[3]-12;
 	}
 	
 	else if(*angle>-90 && *angle <=-60)
 	{
 		// *robot_dir_now = BACKWARD;
 		ratio = -(*angle) / 90;
-		setpoint_m[0] = optimum_speed_m[0]-15;
+		setpoint_m[0] = optimum_speed_m[0]-12;
 		setpoint_m[1] = optimum_speed_m[1]+12;
 		setpoint_m[2] = optimum_speed_m[2]+12;
-		setpoint_m[3] = optimum_speed_m[3]-15;
+		setpoint_m[3] = optimum_speed_m[3]-12;
 	}
 	
 	else if(*angle>-60 && *angle <=-30)
@@ -1105,7 +1115,7 @@ if(*magnitude <30 )
 		setpoint_m[0] = optimum_speed_m[0];
 		setpoint_m[1] = optimum_speed_m[1]+12;
 		setpoint_m[2] = optimum_speed_m[2];
-		setpoint_m[3] = optimum_speed_m[3]-15;
+		setpoint_m[3] = optimum_speed_m[3]-12;
 	}
 	
 	else if(*angle>-30 && *angle <=0)
@@ -1114,8 +1124,8 @@ if(*magnitude <30 )
 		ratio = -(*angle) / 90;
 		setpoint_m[0] = optimum_speed_m[0]+12;
 		setpoint_m[1] = optimum_speed_m[1]+12;
-		setpoint_m[2] = optimum_speed_m[2]-15;
-		setpoint_m[3] = optimum_speed_m[3]-15;
+		setpoint_m[2] = optimum_speed_m[2]-12;
+		setpoint_m[3] = optimum_speed_m[3]-12;
 	}
 	
 	for(int k = 0; k < 4;k++)
@@ -1166,3 +1176,221 @@ if(*magnitude <30 )
 	// if(>0)
 	// {
 		// setpoint_m[0] = optimum_speed_m[0]+round((angle - difference Yaw));
+		
+		
+		
+void ICM20948_SETUP_QUAT6()
+{
+	  WIRE_PORT.begin();
+  WIRE_PORT.setClock(400000);
+
+
+
+  //myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+
+  bool initialized = false;
+  while (!initialized)
+  {
+    myICM.begin(WIRE_PORT, AD0_VAL);
+
+
+    if (myICM.status != ICM_20948_Stat_Ok)
+    {
+
+      SERIAL_PORT.println(F("Trying again..."));
+      delay(500);
+    }
+    else
+    {
+      initialized = true;
+    }
+  }
+
+
+  SERIAL_PORT.println(F("Device connected!"));
+
+  bool success = true; // Use success to show if the DMP configuration was successful
+
+  // Initialize the DMP. initializeDMP is a weak function. You can overwrite it if you want to e.g. to change the sample rate
+  success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok);
+
+  // DMP sensor options are defined in ICM_20948_DMP.h
+  //    INV_ICM20948_SENSOR_ACCELEROMETER               (16-bit accel)
+  //    INV_ICM20948_SENSOR_GYROSCOPE                   (16-bit gyro + 32-bit calibrated gyro)
+  //    INV_ICM20948_SENSOR_RAW_ACCELEROMETER           (16-bit accel)
+  //    INV_ICM20948_SENSOR_RAW_GYROSCOPE               (16-bit gyro + 32-bit calibrated gyro)
+  //    INV_ICM20948_SENSOR_MAGNETIC_FIELD_UNCALIBRATED (16-bit compass)
+  //    INV_ICM20948_SENSOR_GYROSCOPE_UNCALIBRATED      (16-bit gyro)
+  //    INV_ICM20948_SENSOR_STEP_DETECTOR               (Pedometer Step Detector)
+  //    INV_ICM20948_SENSOR_STEP_COUNTER                (Pedometer Step Detector)
+  //    INV_ICM20948_SENSOR_GAME_ROTATION_VECTOR        (32-bit 6-axis quaternion)
+  //    INV_ICM20948_SENSOR_ROTATION_VECTOR             (32-bit 9-axis quaternion + heading accuracy)
+  //    INV_ICM20948_SENSOR_GEOMAGNETIC_ROTATION_VECTOR (32-bit Geomag RV + heading accuracy)
+  //    INV_ICM20948_SENSOR_GEOMAGNETIC_FIELD           (32-bit calibrated compass)
+  //    INV_ICM20948_SENSOR_GRAVITY                     (32-bit 6-axis quaternion)
+  //    INV_ICM20948_SENSOR_LINEAR_ACCELERATION         (16-bit accel + 32-bit 6-axis quaternion)
+  //    INV_ICM20948_SENSOR_ORIENTATION                 (32-bit 9-axis quaternion + heading accuracy)
+
+  // Enable the DMP Game Rotation Vector sensor
+  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_GAME_ROTATION_VECTOR) == ICM_20948_Stat_Ok);
+
+  // Enable any additional sensors / features
+  //success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_GYROSCOPE) == ICM_20948_Stat_Ok);
+  //success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_ACCELEROMETER) == ICM_20948_Stat_Ok);
+  //success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_MAGNETIC_FIELD_UNCALIBRATED) == ICM_20948_Stat_Ok);
+
+  // Configuring DMP to output data at multiple ODRs:
+  // DMP is capable of outputting multiple sensor data at different rates to FIFO.
+  // Setting value can be calculated as follows:
+  // Value = (DMP running rate / ODR ) - 1
+  // E.g. For a 5Hz ODR rate when DMP is running at 55Hz, value = (55/5) - 1 = 10.
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro_Calibr, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass_Calibr, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+
+  // Enable the FIFO
+  success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok);
+
+  // Enable the DMP
+  success &= (myICM.enableDMP() == ICM_20948_Stat_Ok);
+
+  // Reset DMP
+  success &= (myICM.resetDMP() == ICM_20948_Stat_Ok);
+
+  // Reset FIFO
+  success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok);
+
+  // Check success
+  if (success)
+  {
+#ifndef QUAT_ANIMATION
+    SERIAL_PORT.println(F("DMP enabled!"));
+#endif
+  }
+  else
+  {
+    SERIAL_PORT.println(F("Enable DMP failed!"));
+    SERIAL_PORT.println(F("Please check that you have uncommented line 29 (#define ICM_20948_USE_DMP) in ICM_20948_C.h..."));
+    while (1)
+      ; // Do nothing more
+  }
+  
+  if (!EEPROM.begin(128)) // Allocate 128 Bytes for EEPROM storage. ESP32 needs this.
+  {
+    SERIAL_PORT.println(F("EEPROM.begin failed! You will not be able to save the biases..."));
+  }
+
+  biasStore store;
+
+  EEPROM.get(0, store); // Read existing EEPROM, starting at address 0
+  if (isBiasStoreValid(&store))
+  {
+    SERIAL_PORT.println(F("Bias data in EEPROM is valid. Restoring it..."));
+    success &= (myICM.setBiasGyroX(store.biasGyroX) == ICM_20948_Stat_Ok);
+    success &= (myICM.setBiasGyroY(store.biasGyroY) == ICM_20948_Stat_Ok);
+    success &= (myICM.setBiasGyroZ(store.biasGyroZ) == ICM_20948_Stat_Ok);
+    success &= (myICM.setBiasAccelX(store.biasAccelX) == ICM_20948_Stat_Ok);
+    success &= (myICM.setBiasAccelY(store.biasAccelY) == ICM_20948_Stat_Ok);
+    success &= (myICM.setBiasAccelZ(store.biasAccelZ) == ICM_20948_Stat_Ok);
+    success &= (myICM.setBiasCPassX(store.biasCPassX) == ICM_20948_Stat_Ok);
+    success &= (myICM.setBiasCPassY(store.biasCPassY) == ICM_20948_Stat_Ok);
+    success &= (myICM.setBiasCPassZ(store.biasCPassZ) == ICM_20948_Stat_Ok);
+
+    if (success)
+    {
+      SERIAL_PORT.println(F("Biases restored."));
+      printBiases(&store);
+    }
+    else
+      SERIAL_PORT.println(F("Bias restore failed!"));
+  }
+
+
+
+  return;
+}
+
+void ICM20948_GET_READING_QUAT6(double *yaw)
+{
+	double roll,pitch;
+
+	// Read any DMP data waiting in the FIFO
+  // Note:
+  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFONoDataAvail if no data is available.
+  //    If data is available, readDMPdataFromFIFO will attempt to read _one_ frame of DMP data.
+  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFOIncompleteData if a frame was present but was incomplete
+  //    readDMPdataFromFIFO will return ICM_20948_Stat_Ok if a valid frame was read.
+  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFOMoreDataAvail if a valid frame was read _and_ the FIFO contains more (unread) data.
+  icm_20948_DMP_data_t data;
+  myICM.readDMPdataFromFIFO(&data);
+  
+
+  
+
+
+  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
+  {
+    while(myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)
+    {
+      myICM.readDMPdataFromFIFO(&data);
+    }
+    //SERIAL_PORT.print(F("Received data! Header: 0x")); // Print the header in HEX so we can see what data is arriving in the FIFO
+    //if ( data.header < 0x1000) SERIAL_PORT.print( "0" ); // Pad the zeros
+    //if ( data.header < 0x100) SERIAL_PORT.print( "0" );
+    //if ( data.header < 0x10) SERIAL_PORT.print( "0" );
+    //SERIAL_PORT.println( data.header, HEX );
+
+    if ((data.header & DMP_header_bitmap_Quat6) > 0) // We have asked for GRV data so we should receive Quat6
+    {
+      // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
+      // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
+      // The quaternion data is scaled by 2^30.
+
+      //SERIAL_PORT.printf("Quat6 data is: Q1:%ld Q2:%ld Q3:%ld\r\n", data.Quat6.Data.Q1, data.Quat6.Data.Q2, data.Quat6.Data.Q3);
+
+      // Scale to +/- 1
+      double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q3 = ((double)data.Quat6.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
+	  
+      double q2sqr = q2 * q2;
+
+      // roll (x-axis rotation)
+      double t0 = +2.0 * (q0 * q1 + q2 * q3);
+      double t1 = +1.0 - 2.0 * (q1 * q1 + q2sqr);
+      roll = atan2(t0, t1) * 180.0 / PI;
+
+      // pitch (y-axis rotation)
+      double t2 = +2.0 * (q0 * q2 - q3 * q1);
+      t2 = t2 > 1.0 ? 1.0 : t2;
+      t2 = t2 < -1.0 ? -1.0 : t2;
+      pitch = asin(t2) * 180.0 / PI;
+
+      // yaw (z-axis rotation)
+      double t3 = +2.0 * (q0 * q3 + q1 * q2);
+      double t4 = +1.0 - 2.0 * (q2sqr + q3 * q3);
+      *yaw = atan2(t3, t4) * 180.0 / PI;
+	  
+      // SERIAL_PORT.print(F("Roll:"));
+      // SERIAL_PORT.print(roll, 1);
+      // SERIAL_PORT.print(F(" Pitch:"));
+      // SERIAL_PORT.print(pitch, 1);
+      // SERIAL_PORT.print(F(" Yaw:"));
+      // SERIAL_PORT.println(yaw, 1);
+    }
+  }
+
+  
+  if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
+  {
+    delay(10);
+  }
+  
+  return ;
+}	
+
+
